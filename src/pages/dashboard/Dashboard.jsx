14e49f88711ae04b91dbf054589afc4080c1e785
naim <naimulislam0927@gmail.com>
2023-06-10 09:35:33 +0600

    added fav icon

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,21 +1,31 @@
+import { Link } from "react-router-dom";
+import {  FaChalkboardTeacher, FaUser } from 'react-icons/fa';
+import {  SiGoogleclassroom } from 'react-icons/Si';
+import { Helmet } from "react-helmet-async";
 const Dashboard = () => {
     return (
         <div className="flex justify-start">
+            <Helmet>
+                <title>Dashboard | Talenttrek</title>
+            </Helmet>
             <div className="drawer lg:drawer-open">
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content flex flex-col items-start justify-start gap-2 pl-3">
                     {/* Page content here */}
                     <label htmlFor="my-drawer-2" className="btn btn-xs drawer-button lg:hidden">{'>'}</label>
+
                     <div>
-                        hello
+                        
                     </div>
                 </div>
                 <div className="drawer-side">
-                    <label htmlFor="my-drawer-2" className="drawer-overlay">th</label>
-                    <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
+                    <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+                    <ul className="menu p-4 pt-2 w-56 h-full bg-sky-200 child:text-center">
                         {/* Sidebar content here */}
-                        <li><a>Sidebar Item 1</a></li>
-                        <li><a>Sidebar Item 2</a></li>
+                        <li><Link><FaUser/>Profile</Link></li>
+                        <li><Link><FaChalkboardTeacher/>My Classes</Link></li>
+                        <li><Link><SiGoogleclassroom/>Enrolled classes</Link></li>
+
                     </ul>
 
                 </div>
